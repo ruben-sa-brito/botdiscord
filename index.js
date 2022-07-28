7ea76e285069857f6490722722297c5a8021be45
Timewound <rubenjms1@gmail.com>
2022-07-28 01:04:14 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+const Ytdl = require('ytdl-core');
+
+
+const TOKEN = 'OTUwMDMwOTU0OTE3OTQ5NDQw.YiS_Rg.3uF5vsDET-9-a1PMupeMl1tMMFE';
+let estouPronto = false;
+
+client.on("ready", ()=> {
+    console.log('estou online!')
+});
+client.on("message", (msg) => {
+    if(msg.content == 'olá'){
+        msg.reply('olá!');
+    }
+});
+
+client.on("message", (msg) => {
+    if(msg.content === '!join'){
+        if(msg.member.voiceChannel){
+            msg.member.voiceChannel.join()
+            estouPronto = true;
+        }
+        else {
+            msg.channel.send('Voce precisa estar em um canal');
+            
+        }
+    }
+    else if(msg.content === '!sair'){
+        if(msg.member.voiceChannel){
+            msg.member.voiceChannel.leave();
+            estouPronto = false;
+        }
+        else {
+            msg.channel.send('Voce precisa estar em um canal');
+        }
+    }
+    else if(msg.content.startsWith('!play ')){
+        if(estouPronto){
+            let oQueTocar = msg.content.replace('!play ','')
+            
+            if (Ytdl.validateURL(oQueTocar)){
+                msg.member.voiceChannel.connection.playStream(Ytdl(oQueTocar))
+
+            }
+
+            else{
+                msg.channel.send('O link nao é valido');
+            }
+        }
+        }
+    else if(msg.content.startsWith('teste')){
+        msg.member.setMute(true)
+
+
+
+    }});
+//client.on('voiceStateUpdate', (oldMember, newMember) =>{
+//    if (newMember.voiceChannel == "Sala 1"){
+//        newMember.setMute(true)
+//    }
+//    else if(newMember.voiceChannel == oldMember.voiceChannel){
+ //       newMember.setMute(false)
+  //  }
+    
+
+//})
+
+
+client.login(TOKEN);
